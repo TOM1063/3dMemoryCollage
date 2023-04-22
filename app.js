7554e634a110b839632674c1c79b39b7cf2cfad9
TOM1063 <tomshina.dev@gmail.com>
2023-04-22 15:44:59 +0900

    move  funcion

--- app.js
+++ app.js
@@ -46,63 +46,6 @@ let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
 
-function update_freeCamera(target_camera, util, mouse) {
-  const view_arround_factor = 3.14 / 2;
-  let yaw = mouse.x * view_arround_factor * 1.2;
-  let pitch = mouse.y * (-3.14 / 2);
-  let yaw_thresh = 3.14 / 2 - 0.1;
-  if (Math.abs(yaw) < yaw_thresh) {
-    util.head_rot.x = yaw;
-  } else if (yaw < -yaw_thresh) {
-    util.body_rot.x -= 0.01;
-  } else if (yaw > yaw_thresh) {
-    util.body_rot.x += 0.01;
-  }
-  util.rot.x = util.head_rot.x + util.body_rot.x;
-  util.rot.y = pitch;
-
-  util.dir.x = Math.sin(-util.rot.x) * Math.cos(util.rot.y);
-  util.dir.z = Math.cos(util.rot.x) * Math.cos(util.rot.y);
-  util.dir.y = Math.sin(util.rot.y);
-
-  let forward_vel = util.dir;
-  let right_vel = new THREE.Vector3(
-    Math.cos(util.rot.x),
-    0,
-    Math.sin(util.rot.x)
-  );
-  let vel_x =
-    forward_vel.x * Number(moveForward) -
-    forward_vel.x * Number(moveBackward) +
-    right_vel.x * Number(moveRight) -
-    right_vel.x * Number(moveLeft);
-  let vel_z =
-    forward_vel.z * Number(moveForward) -
-    forward_vel.z * Number(moveBackward) +
-    right_vel.z * Number(moveRight) -
-    right_vel.z * Number(moveLeft);
-  let vel_y =
-    forward_vel.y * Number(moveForward) -
-    forward_vel.y * Number(moveBackward) +
-    right_vel.y * Number(moveRight) -
-    right_vel.y * Number(moveLeft);
-  //let vel2 = (right_vel * Number(moveRight)).sub(right_vel * Number(moveLeft));
-  //let vel = vel1.add(vel2) * speed;
-  const motion_factor = 0.5;
-  let speed = motion_factor;
-
-  util.pos.x += vel_x * speed;
-  util.pos.y += vel_y * speed;
-  util.pos.z += vel_z * speed;
-
-  target_camera.position.set(util.pos.x, util.pos.y, util.pos.z);
-  const direction_factor = 1;
-  util.look.x = util.pos.x + util.dir.x * direction_factor;
-  util.look.y = util.pos.y + util.dir.y * direction_factor;
-  util.look.z = util.pos.z + util.dir.z * direction_factor;
-  target_camera.lookAt(util.look.x, util.look.y, util.look.z);
-}
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, size.width / size.height);
 //const camera = new THREE.OrthographicCamera(size.width/-2,size.width/2,size.height/2,size.height/-2,1,10);
@@ -646,6 +589,63 @@ function generateMediaMat(texture, textureSize, windowSize) {
 
   let debug_mat = new THREE.MeshLambertMaterial();
   return glsl_mat;
+}
+
+function update_freeCamera(target_camera, util, mouse) {
+  const view_arround_factor = 3.14 / 2;
+  let yaw = mouse.x * view_arround_factor * 1.2;
+  let pitch = mouse.y * (-3.14 / 2);
+  let yaw_thresh = 3.14 / 2 - 0.1;
+  if (Math.abs(yaw) < yaw_thresh) {
+    util.head_rot.x = yaw;
+  } else if (yaw < -yaw_thresh) {
+    util.body_rot.x -= 0.01;
+  } else if (yaw > yaw_thresh) {
+    util.body_rot.x += 0.01;
+  }
+  util.rot.x = util.head_rot.x + util.body_rot.x;
+  util.rot.y = pitch;
+
+  util.dir.x = Math.sin(-util.rot.x) * Math.cos(util.rot.y);
+  util.dir.z = Math.cos(util.rot.x) * Math.cos(util.rot.y);
+  util.dir.y = Math.sin(util.rot.y);
+
+  let forward_vel = util.dir;
+  let right_vel = new THREE.Vector3(
+    Math.cos(util.rot.x),
+    0,
+    Math.sin(util.rot.x)
+  );
+  let vel_x =
+    forward_vel.x * Number(moveForward) -
+    forward_vel.x * Number(moveBackward) +
+    right_vel.x * Number(moveRight) -
+    right_vel.x * Number(moveLeft);
+  let vel_z =
+    forward_vel.z * Number(moveForward) -
+    forward_vel.z * Number(moveBackward) +
+    right_vel.z * Number(moveRight) -
+    right_vel.z * Number(moveLeft);
+  let vel_y =
+    forward_vel.y * Number(moveForward) -
+    forward_vel.y * Number(moveBackward) +
+    right_vel.y * Number(moveRight) -
+    right_vel.y * Number(moveLeft);
+  //let vel2 = (right_vel * Number(moveRight)).sub(right_vel * Number(moveLeft));
+  //let vel = vel1.add(vel2) * speed;
+  const motion_factor = 0.5;
+  let speed = motion_factor;
+
+  util.pos.x += vel_x * speed;
+  util.pos.y += vel_y * speed;
+  util.pos.z += vel_z * speed;
+
+  target_camera.position.set(util.pos.x, util.pos.y, util.pos.z);
+  const direction_factor = 1;
+  util.look.x = util.pos.x + util.dir.x * direction_factor;
+  util.look.y = util.pos.y + util.dir.y * direction_factor;
+  util.look.z = util.pos.z + util.dir.z * direction_factor;
+  target_camera.lookAt(util.look.x, util.look.y, util.look.z);
 }
 
 window.addEventListener("resize", () => {
